e9da76e1dd6e35a6a16f801f651f3f7590ee6588
VishnuSanal <t.v.s10123@gmail.com>
2024-04-23 17:48:26 +0530

    fix A13 permission

--- app/src/main/java/phone/vishnu/notifdict/MainActivity.kt
+++ app/src/main/java/phone/vishnu/notifdict/MainActivity.kt
@@ -1,28 +1,66 @@
 package phone.vishnu.notifdict
 
-import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import org.json.JSONArray
 
-class MainActivity : Activity() {
+
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ContextCompat.checkSelfPermission(
+                this, android.Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                startActivity(
+                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(
+                            Uri.fromParts(
+                                "package", packageName, null
+                            )
+                        )
+                )
+                quit("Please enable notification permission from the settings")
+            }
+
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                Log.e("vishnu", "onCreate() called with: isGranted = $isGranted")
+                if (isGranted) {
+                    fetchMeaning(intent);
+                } else {
+                    quit("Can't post notifications without Notification permission")
+                }
+            }.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+
         val action = intent.action
 
-        if (action == null ||
-            (action != Intent.ACTION_PROCESS_TEXT &&
-                    action != Intent.ACTION_SEND)
-        ) return
+        if (action == null || (action != Intent.ACTION_PROCESS_TEXT && action != Intent.ACTION_SEND)) return
 
-        var word = intent.extras?.getString(Intent.EXTRA_PROCESS_TEXT, null) ?:
-                            intent.extras?.getString(Intent.EXTRA_TEXT, null)
+        fetchMeaning(intent)
+    }
+
+    private fun fetchMeaning(intent: Intent) {
+        var word =
+            intent.extras?.getString(Intent.EXTRA_PROCESS_TEXT, null) ?: intent.extras?.getString(
+                Intent.EXTRA_TEXT, null
+            )
 
         if (word == null) {
             quit(getString(R.string.please_select_a_single_word))
@@ -36,9 +74,7 @@ class MainActivity : Activity() {
         val url = "https://api.dictionaryapi.dev/api/v2/entries/en/$word"
 
         NotifDictController.getInstance().addToRequestQueue(
-            StringRequest(
-                Request.Method.GET, url,
-                { response ->
+                StringRequest(Request.Method.GET, url, { response ->
 
                     val meaningsArray =
                         JSONArray(response).getJSONObject(0).getJSONArray("meanings");
@@ -49,8 +85,7 @@ class MainActivity : Activity() {
 
                         val definition = meaningsArray.getJSONObject(i)
 
-                        val definitionsArray =
-                            definition.getJSONArray("definitions")
+                        val definitionsArray = definition.getJSONArray("definitions")
 
                         stringBuilder.append(
                             "${definition.getString("partOfSpeech")}:\n"
@@ -72,15 +107,14 @@ class MainActivity : Activity() {
                         this, word, stringBuilder.toString()
                     )
 
-                },
-                {
+                }, {
                     if (it.networkResponse.statusCode == 404) {
                         quit("Word not found!")
                     } else {
                         quit("Whoops! Something went wrong!")
                     }
                 })
-        )
+            )
 
         finish()
     }
