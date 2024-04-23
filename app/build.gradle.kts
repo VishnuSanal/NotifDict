b38a3b900071e57680bd2b0f291a18b09a9e6259
VishnuSanal <t.v.s10123@gmail.com>
2024-04-23 17:50:51 +0530

    v1.0.1 => update version & metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "phone.vishnu.notifdict"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
